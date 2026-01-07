6f2b5eeae89202df52463a9adeac8f5f52f508d0
Gentjana <hanigentjana@gmail.com>
2026-01-07 19:44:13 +0100

    adds

--- src/es1/AnnoBisestile.java
+++ src/es1/AnnoBisestile.java
@@ -3,18 +3,22 @@ package es1;
 import java.util.Scanner;
 
 public class AnnoBisestile {
+    public static boolean annoBisestile(int year) {
+
+
+        if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Inserisci un anno qualsiasi");
         int year = sc.nextInt();
-        boolean annoBisestile;
-        if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
-            annoBisestile = true;
-        } else {
-            annoBisestile = false;
-        }
 
-        System.out.println("L'anno inserito è bisestile?" + " " + annoBisestile);
+        System.out.println("L'anno inserito è bisestile?" + " " + annoBisestile(year));
     }
 }
--- src/es1/StringaPariDispari.java
+++ src/es1/StringaPariDispari.java
@@ -4,23 +4,28 @@ package es1;
 import java.util.Scanner;
 
 public class StringaPariDispari {
+    public static boolean stringaPariDispari(String text) {
+
+        int numeroCaratteri = text.length();
+
+
+        if (numeroCaratteri % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Scrivi il tuo nome");
-        String name = sc.nextLine();
+        String text = sc.nextLine();
 
-        int numeroCaratteri = name.length();
-        boolean res;
 
-        if (numeroCaratteri % 2 == 0) {
-            res = true;
-        } else {
-            res = false;
-        }
-        System.out.println("Il numero di caratteri è pari?" + " " + res);
-        System.out.println("Il nome è lungo" + " " + numeroCaratteri + " " + "caratteri.");
+        System.out.println("Il numero di caratteri è pari?" + " " + stringaPariDispari(text));
+        System.out.println("Il nome è lungo" + " " + text.length() + " " + "caratteri.");
 
 
     }
